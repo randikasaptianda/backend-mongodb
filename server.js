98d08f2761cfb4b5a6b9adb17ce9e163ff3a0fa6
DESKTOP-1DJ5NF7\Lenovo <randikasaptianda28@gmail.com>
2022-05-23 23:55:37 +0700

    add backend mongodb

---
+++ server.js
@@ -0,0 +1,10 @@
+const app = require("./index");
+const dotenv = require("dotenv");
+const connectDatabase = require("./config/database");
+
+dotenv.config({ path: "config/.env" });
+
+connectDatabase();
+app.listen(process.env.PORT, () => {
+  console.log("server is working on http://localhost:${process.env.PORT}");
+});
